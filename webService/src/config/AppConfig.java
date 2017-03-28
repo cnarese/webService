1220354f64ff8b620b2227301b183de504f39ca1
cnarese <cnarese@MED-108-143.unito.ADunito.it>
2017-03-28 10:37:25 +0200

    FIRST

---
+++ webService/src/config/AppConfig.java
@@ -0,0 +1,37 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import controller.CustomerRestController;
+import dao.CustomerDAO;
+
+@Configuration
+@EnableWebMvc
+//@ComponentScan(basePackages = "model")
+//@ComponentScan(basePackages = "controller")
+
+@ComponentScan(basePackageClasses = {CustomerDAO.class, CustomerRestController.class})
+public class AppConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
---
+++ webService/src/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.security.web.context.*;
+
+public class SecurityWebApplicationInitializer
+      extends AbstractSecurityWebApplicationInitializer {
+
+    public SecurityWebApplicationInitializer() {
+        super(SecurityConfig.class);
+    }
+}
