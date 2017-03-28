1d2258be549573ebbe18691455f1a2a66e767136
cnarese <cnarese@MED-108-143.unito.ADunito.it>
2017-03-28 13:45:16 +0200

    Funzionante con profilatura

--- webService/src/config/SecurityConfig.java
+++ webService/src/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -23,8 +26,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	  http.authorizeRequests()
 		.antMatchers("/").permitAll()
-		.antMatchers("/customers/**").access("hasRole('ROLE_USER')")
-		.and().formLogin();
+		.antMatchers("/customers/**").access("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+		.and().formLogin()
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+/*		.and().logout()
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/");*/
+	  	
 
 	}
 }
